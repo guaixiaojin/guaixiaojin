980eccdb845e9bc27611dc7620a60088678ff0c2
怪小金 <8721759+strange-little-gold@user.noreply.gitee.com>
2022-11-11 16:24:33 +0800

    添加最新的作品

---
+++ vuedemo/qsbk/dist/assets/AboutView.13428890.js
@@ -0,0 +1,1 @@
+import{_ as e,e as s,h as t,j as o}from"./index.3c14ef27.js";const c={},a={class:"about"},n=o("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),t("div",a,_)}const l=e(c,[["render",r]]);export{l as default};
